74d6ec5b2e7afaa67685670964cbddd2f26cf1fa
Carl Eiserman <carleiserman@gmail.com>
2019-02-17 13:39:38 +0200

    refactor: removed startup component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as CurrentRoll } from './current-roll';
 export { default as SelectField } from './select-field';
-export { default as Startup } from './startup';
 export { default as StatsDisplay } from './stats-display';
