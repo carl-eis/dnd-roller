675f8c01dd2c3ef2ce9182f3a21134079ab260d0
Carl Eiserman <carleiserman@gmail.com>
2019-02-17 13:48:03 +0200

    refactor: remove unneeded tests

--- src/pages/home/helpers/get-stats-obj/index.test.ts
+++
@@ -1,9 +0,0 @@
-import getStatsObj from './';
-
-describe('get stats obj', () => {
-  it('should do some stuff', () => {
-    const result = getStatsObj();
-    console.log(JSON.stringify(result, null, 2));
-    expect(result).toBeDefined();
-  });
-});
