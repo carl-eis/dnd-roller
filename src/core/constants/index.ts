e7dfa591edbce68292bcc4cb0bbe8cce00dfedc0
Carl Eiserman <carleiserman@gmail.com>
2019-02-26 22:29:53 +0200

    feat: added ruleset definitions

---
+++ src/core/constants/index.ts
@@ -0,0 +1,35 @@
+interface IDiceRuleset {
+  name: string;
+  dice: number;
+  discardDice: number;
+  discardSets: number;
+  sets: number;
+}
+
+interface IRulesets {
+  [x: string]: IDiceRuleset;
+}
+
+export const RULESETS: IRulesets = {
+  ROLL_3: {
+    name: 'Roll 3',
+    dice: 3,
+    sets: 6,
+    discardDice: 0,
+    discardSets: 0,
+  },
+  ROLL_3_DISCARD_SET: {
+    name: 'Roll 7x3, discard lowest set',
+    dice: 3,
+    sets: 7,
+    discardDice: 0,
+    discardSets: 1,
+  },
+  ROLL_4_DISCARD_1: {
+    name: 'Roll 6x4, discard lowest dice',
+    dice: 4,
+    sets: 6,
+    discardDice: 1,
+    discardSets: 0,
+  },
+};
